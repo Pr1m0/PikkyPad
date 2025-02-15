9445609756b1fb91170f868bedefa05b1c16c301
Heszler István <heszleristvan@ktch.hu>
2025-02-15 21:46:19 +0100

    Adatbázis&Bejelentkezés

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,12 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChildController;
-
+use App\Http\Controllers\AuthController;
 
 Route::get('/children', [ChildController::class, 'index']);
 Route::post('/children', [ChildController::class, 'store']);
 Route::put('/children/{id}', [ChildController::class, 'update']);
 Route::delete('/children/{id}', [ChildController::class, 'destroy']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
