2b0df38df62372907154fe28fbbd2a626a80f99d
Heszler István <heszleristvan@ktch.hu>
2025-02-16 12:53:08 +0100

    Child&AuthContoller_Sanctum_api_config/database

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,18 +1,25 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ChildController;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ChildController;
+use App\Http\Controllers\Api\GameController;
+use App\Http\Controllers\Api\StatisticsController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/children', [ChildController::class, 'index']);
 Route::post('/children', [ChildController::class, 'store']);
 Route::put('/children/{id}', [ChildController::class, 'update']);
 Route::delete('/children/{id}', [ChildController::class, 'destroy']);
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+});
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
