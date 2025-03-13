54edd1d27c88be89e1b79f1a1617a4b03efc1a71
Tanulo <you@example.com>
2025-03-13 19:09:56 +0100

    Összekötés

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -15,27 +15,45 @@ import { AppComponent } from '../app.component';
 })
 export class LoginComponent {
 
-  constructor(
-    private authService:AuthService,
-    private builder:FormBuilder,
-    private app:AppComponent){}
-  ngOnInit() {
+  // constructor(
+  //   private authService:AuthService,
+  //   private builder:FormBuilder,
+  //   private app:AppComponent){}
+  // ngOnInit() {
 
-    this.authService.login("","").subscribe({
-      next :(body: any) => {
-        console.log(body.data.token);
-        localStorage.setItem('token', body.data.token);
+  //   this.authService.login("","").subscribe({
+  //     next :(body: any) => {
+  //       console.log(body.data.token);
+  //       localStorage.setItem('token', body.data.token);
    
-      },
-      error:(err) => {
-        console.log(err);
-      }
-    })
-  }
-  login(){
+  //     },
+  //     error:(err) => {
+  //       console.log(err);
+  //     }
+  //   })
+  // }
+  // login(){
   
     
+  // }
+  user = {
+    name: '',
+    password: ''
+  };
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onSubmit() {
+    this.http.post('http://localhost:8000/api/login', this.user)
+      .subscribe((response: any) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/home-private']); // Átirányítás a védett oldalra
+      // }, error => {
+      //   console.log(error);
+       }
+      );
   }
 }
+
 
 
--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -23,27 +23,25 @@ constructor(private builder: FormBuilder, private http:HttpClient, private route
     name:['', [Validators.required, Validators.minLength(5)]],
     email:['', [Validators.required, Validators.email]],
     password:['', [Validators.required, Validators.minLength(8)]],
-    childname:['', Validators.required],
-    childage: [0, [Validators.required, Validators.min(0)]]  //, Validators.max(18)
+    // childname:['', Validators.required],
+    // childage: [0, [Validators.required, Validators.min(0)]]  //, Validators.max(18)
 })
 
  
 }
  
 register(){
-  // if(this.registerForm.valid){
-  //   this.http.post('http://localhost:4200/register', this.registerForm.value)
-  //       .subscribe(responnse =>{
+   if(this.registerForm.valid){
+    this.http.post('http://localhost:8000/api/register', this.registerForm.value)
+        .subscribe(responnse =>{
           console.log("Sikeres regisztráció" , this.registerForm.value,
             this.router.navigate(['/home-private']),
             this.dataService.setData(this.registerForm.value)// Add to local storagedataService.setData(this.registerForm.value); // Add to local storage
             
            
           );
-  //       }, error => {
-  //         console.log("Hiba a regisztráció során", error);
-  //       });
-  // }
+         }, );
+   }
 }
 
 }
