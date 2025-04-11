989c0421c388cd1d783599fc50c0dec31751740a
Heszler István <heszleristvan@ktch.hu>
2025-04-11 15:45:00 +0200

    Frontend törölve

--- frontend/src/app/auth.guard.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
-
-
-
-
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
